6c97fb2e94b494cc487ee361304dfa6459bb2142
luff1012 <suyash1212@gmail.com>
2016-04-05 22:35:23 +0530

    First working version

--- html_front_end/flashsubs_app/views.py
+++ html_front_end/flashsubs_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import flashsub
+import os
 
 def main_page(request):
-    return render(request,"flashsubs_app/test.html")
+    if request.method == "POST":
+        path = request.POST.get('path')
+        user = request.POST.get('user')
+        passwd = request.POST.get('password')
+        proxy = request.POST.get('proxy')
+        if os.path.isdir(path):
+            flashsub.run(path,proxy,user,passwd)
+            return HttpResponse("Done")
+        else:       
+            return HttpResponse("Enter correct path")
+    return render(request,"flashsubs_app/main.html")
